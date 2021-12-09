9d6ab00303c76a484089bac9aaea4670f3776001
James Black <james@epijim.uk>
2021-12-09 20:26:51 +0100

    add code to generate badges

---
+++ generate-badges.R
@@ -0,0 +1,136 @@
+library(glue)
+library(dplyr)
+
+## Load
+
+commits <- readRDS("scratch/commits.rds")
+repos <- readRDS("scratch/repos.rds")
+
+## Write badges
+
+template <- "https://img.shields.io/badge/{label}-{value}-{colour}"
+
+badges <- repos %>%
+  mutate(
+    badge_cran = case_when(
+      is.na(cran_version) ~ "",
+      TRUE ~ as.character(glue(
+        template, 
+        label = "CRAN",
+        colour = "9cf",
+        value = cran_version
+      ))
+    ),
+    badge_contributors = case_when(
+      is.na(Contributors) ~ "",
+      TRUE ~ as.character(glue(
+        template, 
+        label = "Contributors",
+        colour = "9cf",
+        value = Contributors
+      ))
+    ),
+    badge_health = case_when(
+      is.na(Health) ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "red",
+        value = 0
+      )),
+      Health > 90 ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "brightgreen",
+        value = Health
+      )),
+      Health > 80 ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "green",
+        value = Health
+      )),
+      Health > 60 ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "yellowgreen",
+        value = Health
+      )),
+      Health > 50 ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "yellow",
+        value = Health
+      )),
+      Health > 40 ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "orange",
+        value = Health
+      )),
+      TRUE ~ as.character(glue(
+        template, 
+        label = "Health",
+        colour = "red",
+        value = Health
+      ))
+    )
+  ) %>%
+  select(
+    full_name,
+    badge_cran,
+    badge_contributors,
+    badge_health
+  )
+
+
+
+## Upload
+
+write.csv(badges, glue("scratch/badges-{Sys.Date()}.csv"), row.names = FALSE)
+write.csv(badges, glue("scratch/badges.csv"), row.names = FALSE)
+
+library(aws.s3)
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = Sys.getenv("OPENPHARMA_AWS_ACCESS_KEY_ID"),
+  "AWS_SECRET_ACCESS_KEY" = Sys.getenv("OPENPHARMA_AWS_SECRET_ACCESS_KEY"),
+  "AWS_DEFAULT_REGION" = Sys.getenv("OPENPHARMA_AWS_DEFAULT_REGION")
+)
+
+# Write new files
+to_upload <- c(
+  "badges"
+)
+
+if (format(Sys.Date(), format = "%d") == "1") {
+  for (i in to_upload) {
+    put_object(
+      file = glue("scratch/{i}-{Sys.Date()}.csv"), 
+      object = glue("{i}-{Sys.Date()}.csv"), 
+      bucket = "openpharma",verbose = FALSE
+    )
+  }
+  
+  # for (i in to_upload) {
+  #   put_object(
+  #     file = glue("scratch/{i}-{Sys.Date()}.rds"), 
+  #     object = glue("{i}-{Sys.Date()}.rds"), 
+  #     bucket = "openpharma",verbose = FALSE
+  #   )
+  # }
+}
+
+for (i in to_upload) {
+  put_object(
+    file = glue("scratch/{i}.csv"), 
+    object = glue("{i}.csv"), 
+    bucket = "openpharma",verbose = FALSE
+  )
+}
+
+# for (i in to_upload) {
+#   put_object(
+#     file = glue("scratch/{i}.rds"), 
+#     object = glue("{i}.rds"), 
+#     bucket = "openpharma",verbose = FALSE
+#   )
+# }

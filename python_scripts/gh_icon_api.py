f19c136cb078c3d8b881877fd8ef7f80b2df76e5
MathieuCayssol <mathieu.cayssol@gmail.com>
2022-06-17 16:40:00 +0200

    trying with Pat

--- python_scripts/gh_icon_api.py
+++ python_scripts/gh_icon_api.py
@@ -8,7 +8,7 @@ from requests.packages.urllib3.util.retry import Retry
 #another try
 
 GITHUB_URL_ICON = 'https://api.github.com/repos/pharmaverse/pharmaverse/contents/data/packages'
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+OPENPHARMA_PAT = os.getenv('OPENPHARMA_PAT')
 
 
 def get_icon_package_gh_api(path: str=GITHUB_URL_ICON):
@@ -18,12 +18,12 @@ def get_icon_package_gh_api(path: str=GITHUB_URL_ICON):
         adapter = HTTPAdapter(max_retries=retry)
         session.mount('http://', adapter)
         session.mount('https://', adapter)
-        response = requests.get(path, headers = {'Authorization': 'token {}'.format(GITHUB_TOKEN)})
+        response = requests.get(path, auth=('epijim',OPENPHARMA_PAT))
         json_packages = [response.json()[i]['download_url'] for i in range(len(response.json()))]
         l_repo = []
         l_icon = []
         for x in json_packages:
-            yaml_file = yaml.load(requests.get(x, headers = {'Authorization': 'token {}'.format(GITHUB_TOKEN)}).text)
+            yaml_file = yaml.load(requests.get(x, auth=('epijim',OPENPHARMA_PAT)).text)
             if (yaml_file['repo'] != None & yaml_file['hex'] != None):
                 l_repo.append(yaml_file['repo'])
                 l_icon.append(yaml_file['hex'])

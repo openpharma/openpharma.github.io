ec13f6f5c466208a0bfbaec576dc9bcc726dc4fd
MathieuCayssol <mathieu.cayssol@gmail.com>
2022-06-17 10:40:27 +0200

    Next try with github token authentification

--- python_scripts/gh_icon_api.py
+++ python_scripts/gh_icon_api.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import yaml
 import requests
@@ -6,6 +7,8 @@ from requests.packages.urllib3.util.retry import Retry
 
 
 GITHUB_URL_ICON = 'https://api.github.com/repos/pharmaverse/pharmaverse/contents/data/packages'
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
 
 def get_icon_package_gh_api(path: str=GITHUB_URL_ICON):
     try:
@@ -14,13 +17,13 @@ def get_icon_package_gh_api(path: str=GITHUB_URL_ICON):
         adapter = HTTPAdapter(max_retries=retry)
         session.mount('http://', adapter)
         session.mount('https://', adapter)
-        response = requests.get(path)
+        response = requests.get(path, headers = {'Authorization': 'token {}'.format(GITHUB_TOKEN)})
         json_packages = [response.json()[i]['download_url'] for i in range(len(response.json()))]
         l_repo = []
         l_icon = []
         for x in json_packages:
-            yaml_file = yaml.load(requests.get(x).text)
-            if (yaml_file['repo'] != None and yaml_file['hex'] != None):
+            yaml_file = yaml.load(requests.get(x, headers = {'Authorization': 'token {}'.format(GITHUB_TOKEN)}).text)
+            if (yaml_file['repo'] != None & yaml_file['hex'] != None):
                 l_repo.append(yaml_file['repo'])
                 l_icon.append(yaml_file['hex'])
         d = {'full_name': l_repo, 'icon_package': l_icon}

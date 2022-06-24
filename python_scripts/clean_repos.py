2f8cbb97197b4d9be137028c82b047ab09a777ab
MathieuCayssol <mathieu.cayssol@gmail.com>
2022-06-24 17:26:56 +0200

    Small modif on icon img...

--- python_scripts/clean_repos.py
+++ python_scripts/clean_repos.py
@@ -31,16 +31,17 @@ def clean_merge_df(path: str) -> pd.DataFrame:
     df['lang'] = df['lang'].fillna('R')
     df['cran_license'] = df['cran_license'].fillna('Other Licenses')
     df['license_clean'] = df['cran_license'].apply(clean_license)
-    df['risk_column'] = (20*df['riskmetric_score_quintile']+df['os_health'])
-    df['risk_column'] = (100*df['risk_column']/df['risk_column'].max())
+    df['risk_column'] = (20*df['riskmetric_score_quintile']+df['os_health'])/2
     df['last_commit_d'] = (pd.to_datetime("today") - pd.to_datetime(df['Last Commit'])).dt.days.astype('Int64')
 
     df_icon = gh_icon_api.get_icon_package_gh_api()
     if(len(df_icon)>=1):
         df = df.merge(df_icon, how='left', on='full_name', suffixes=('', '_'))
-        df['icon_package'] = df['icon_package'].fillna("https://cran.r-project.org/Rlogo.svg")
+        df['icon_package'] = df['icon_package'].fillna("https://openpharma.s3.us-east-2.amazonaws.com/streamlit_img/Rlogo.svg")
+        df.loc[df['lang'] == "python", 'icon_package'] = "https://openpharma.s3.us-east-2.amazonaws.com/streamlit_img/python.png"
     else:
         df['icon_package'] = "https://cran.r-project.org/Rlogo.svg"
+        df.loc[df['lang'] == "python", 'icon_package'] = "https://openpharma.s3.us-east-2.amazonaws.com/streamlit_img/python.png"
     return df
 
 

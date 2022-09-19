81224c9395bc40d4baad89ce054c5f9a9afcc443
Mathieu Cayssol <mathieu.cayssol@gmail.com>
2022-09-19 11:59:02 +0200

    R icon

--- python_scripts/clean_openissues.py
+++ python_scripts/clean_openissues.py
@@ -6,7 +6,7 @@ def clean_merge_df(path: str) -> pd.DataFrame:
     df_icon = gh_icon_api.get_icon_package_gh_api()
     if(len(df_icon)>=1):
         df = df.merge(df_icon, how='left', on='full_name', suffixes=('', '_'))
-        df['icon_package'] = df['icon_package'].fillna("https://cran.r-project.org/Rlogo.svg")
+        df['icon_package'] = df['icon_package'].fillna("https://openpharma.s3.us-east-2.amazonaws.com/streamlit_img/Rlogo.svg")
     else:
-        df['icon_package'] = "https://cran.r-project.org/Rlogo.svg"
+        df['icon_package'] = "https://openpharma.s3.us-east-2.amazonaws.com/streamlit_img/Rlogo.svg"
     return df
